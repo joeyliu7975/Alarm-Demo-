09bf5948aee16041be9aea69bd2e9cbc38c63f4e
Joey Liu <joeyliu@Joeys-MacBook-Pro.local>
2020-03-09 10:30:32 +0800

    App setup

---
+++ Alarm Demo/Alarm Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarm Demo
+//
+//  Created by Joey Liu on 3/9/20.
+//  Copyright © 2020 Joey Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
